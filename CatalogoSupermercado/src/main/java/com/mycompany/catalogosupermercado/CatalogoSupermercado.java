5dce0c9f91ee2766b095d043cb5e55096828ad87
maurelo860@gmail.com <maurelo860@gmail.com>
2026-04-25 15:59:32 -0600

    Proyecto Principal

---
+++ CatalogoSupermercado/src/main/java/com/mycompany/catalogosupermercado/CatalogoSupermercado.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.catalogosupermercado;
+
+/**
+ *
+ * @author mauricio
+ */
+public class CatalogoSupermercado {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
